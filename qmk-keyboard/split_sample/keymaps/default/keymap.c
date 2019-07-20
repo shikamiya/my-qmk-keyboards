930f0b20dfd659e4410a9585b1b04ba6e0021f9c
shikamiya <46595896+shikamiya@users.noreply.github.com>
2019-07-20 12:01:18 +0900

    Change include of keymap

--- qmk-keyboard/split_sample/keymaps/default/keymap.c
+++ qmk-keyboard/split_sample/keymaps/default/keymap.c
@@ -14,10 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
-
-#ifdef PROTOCOL_LUFA
-  #include "lufa.h"
-#endif
+#include <stdio.h>
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
